18bf24a5ee1289d450bcefd1c199116dd23fec0e
Timur Begishev <rpsi.timurec@gmail.com>
2022-04-09 15:35:03 +0300

    Добавлен скрипт для инициализации проекта

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+git submodule update --init
+cd submodules/SwiftSemantics && git checkout 7690606eec5db6b089d6a5d252013ee07cade323
+
+local MMDC="$(which mmdc)"
+if [ "$MMDC" = "" ]; then
+	echo "Mermaid-cli not found in PATH"
+	echo "Please install it: brew install mermaid-cli"
+	exit 1
+fi
